3230ad7924bb33d37a043d8bab700b2fe7567b1d
Patricia <paty4paty@gmail.com>
2019-09-06 17:49:05 -0400

    Inicio proyecto

---
+++ funciones.py
@@ -0,0 +1,18 @@
+# Universidad de Santiago de Chile
+# Laboratorio 1 de Redes de Computadores
+# Autores: Williams Álvarez y Patricia Melo.
+# Versión 1.
+
+
+# IMPORTAR LIRERÍAS
+import matplotlib.pylab as plt
+import scipy as sp
+import numpy as np
+
+
+# DEFINICIÓN DE CONSTANTES
+
+# DEFINICIÓN DE FUNCIONES
+
+
+print("hola")
