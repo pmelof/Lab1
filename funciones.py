c0a660adc994d57c1305615055e9d3113da3199b
Unknown <williams.alvarez@usach.cl>
2019-09-06 18:07:09 -0400

    cambios menores

--- funciones.py
+++ funciones.py
@@ -8,20 +8,23 @@
 import matplotlib.pylab as plt
 import scipy as sp
 import scipy.io.wavfile as waves
+import wave
 import numpy as np
 
 # DEFINICIÓN DE CONSTANTES
 
 # DEFINICIÓN DE FUNCIONES
+def graphSong(dataRead, sampleRate):
+    time = np.linspace(0, len(dataRead) / sampleRate, num=len(dataRead))
+    plt.plot(time, dataRead, color='y')
+    plt.title('Amplitud v/s Tiempo', fontdict={'size': 15})
+    plt.xlabel('Tiempo [s]')
+    plt.ylabel('Amplitud')
+    plt.show()
+
+def main():
+    sampleRate, dataRead = waves.read('handel.wav')
+    graphSong(dataRead, sampleRate)
 
 # INGRESO
-
-muestreo, sonido = waves.read('handel.wav')
-
-plt.plot(sonido)
-plt.title('Sonido')
-plt.xlabel('Tiempo [s]')
-plt.ylabel('Algo')
-plt.show()
-
-print("hola")
+main();
