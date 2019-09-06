79f3bbd3c9b871ec2f4036cae542467808819c6c
Unknown <williams.alvarez@usach.cl>
2019-09-06 14:01:02 -0400

    avance 1

--- funciones.py
+++ funciones.py
@@ -7,12 +7,21 @@
 # IMPORTAR LIRERÍAS
 import matplotlib.pylab as plt
 import scipy as sp
+import scipy.io.wavfile as waves
 import numpy as np
-
 
 # DEFINICIÓN DE CONSTANTES
 
 # DEFINICIÓN DE FUNCIONES
 
+# INGRESO
+
+muestreo, sonido = waves.read('handel.wav')
+
+plt.plot(sonido)
+plt.title('Sonido')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Algo')
+plt.show()
 
 print("hola")
