2b76fc6e90150094e15fa6a5914b7d7d8e4a146c
Patricia <paty4paty@gmail.com>
2019-09-10 19:27:23 -0300

    Casi listo

--- funciones.py
+++ funciones.py
@@ -6,7 +6,7 @@
 
 # IMPORTAR LIRERÍAS
 import matplotlib.pylab as plt
-from scipy.fftpack import fft
+from scipy.fftpack import fft, fftfreq, ifft
 from scipy import fftpack
 import scipy as sp
 import scipy.io.wavfile as waves
@@ -16,24 +16,51 @@ import numpy as np
 # DEFINICIÓN DE CONSTANTES
 
 # DEFINICIÓN DE FUNCIONES
-def graphSong(dataRead, sampleRate):
-    time = np.linspace(0, len(dataRead) / sampleRate, num=len(dataRead))
-    plt.plot(time, dataRead, color='y')
+def graphSong(data_read, sample_rate):
+    time = np.linspace(0, len(data_read) / sample_rate, num=len(data_read))
+    plt.plot(time, data_read, color='y')
     plt.title('Amplitud v/s Tiempo', fontdict={'size': 15})
     plt.xlabel('Tiempo [s]')
     plt.ylabel('Amplitud')
     plt.show()
 
-def dffGraphSong(dataRead, sampleRate):
-    time = np.linspace(0, len(dataRead) / sampleRate, num=len(dataRead))
-    fft_out = fft(dataRead)
-    plt.plot(time, np.abs(fft_out))
+
+def fftGraphSong(data_read, sample_rate):
+    fft_out = fft(data_read)
+    fft_freq = fftfreq(len(data_read), 1/sample_rate)
+    plt.plot(fft_freq, np.abs(fft_out))
+    plt.title("Transformada de Fourier de la canción")
+    plt.xlabel("Frecuencia")
+    plt.ylabel("|F(w)|")
     plt.show()
+    truncateGraphSong(fft_out, fft_freq, sample_rate)
+
+
+def invFftFileSong(fft_out, sample_rate, name_file):
+    ifft_out = ifft(fft_out)
+    plt.plot(ifft_out.real)
+    plt.show()
+    waves.write(name_file, sample_rate, ifft_out.real)
+
+
+def truncateGraphSong(fft_out, fft_freq, sample_rate):
+    #centro 36556+-1
+    fft_out[16556:56557] = 0
+    plt.plot(fft_freq, np.abs(fft_out))
+    plt.title("Transformada de Fourier de la canción (truncado)")
+    plt.xlabel("Frecuencia")
+    plt.ylabel("|F(w)|")
+    plt.show()
+    invFftFileSong(fft_out, sample_rate, 'truncado.wav')
+
 
 def main():
-    sampleRate, dataRead = waves.read('handel.wav')
-    graphSong(dataRead, sampleRate)
-    dffGraphSong(dataRead, sampleRate)
+    sample_rate, data_read = waves.read('handel.wav')
+    #print(sample_rate)
+    #print(len(data_read))
+    graphSong(data_read, sample_rate)
+    fftGraphSong(data_read, sample_rate)
+
 
 
 # INGRESO
