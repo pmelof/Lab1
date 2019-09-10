19c515a3cde2a2a0ac4d136139fbf167eb0854be
Unknown <williams.alvarez@usach.cl>
2019-09-10 20:40:03 -0300

    Varios cambios

--- funciones.py
+++ funciones.py
@@ -1,22 +1,17 @@
 # Universidad de Santiago de Chile
 # Laboratorio 1 de Redes de Computadores
 # Autores: Williams Álvarez y Patricia Melo.
-# Versión 1.
 
 
-# IMPORTAR LIRERÍAS
+# IMPORT LIBRARIES
 import matplotlib.pylab as plt
 from scipy.fftpack import fft, fftfreq, ifft
-from scipy import fftpack
-import scipy as sp
 import scipy.io.wavfile as waves
-import wave
 import numpy as np
 
-# DEFINICIÓN DE CONSTANTES
 
-# DEFINICIÓN DE FUNCIONES
-def graphSong(data_read, sample_rate):
+# FUNCTION DEFINITION
+def sound_graph(data_read, sample_rate):
     time = np.linspace(0, len(data_read) / sample_rate, num=len(data_read))
     plt.plot(time, data_read, color='y')
     plt.title('Amplitud v/s Tiempo', fontdict={'size': 15})
@@ -25,43 +20,49 @@ def graphSong(data_read, sample_rate):
     plt.show()
 
 
-def fftGraphSong(data_read, sample_rate):
+def sound_freq_graph(data_read, sample_rate):
     fft_out = fft(data_read)
-    fft_freq = fftfreq(len(data_read), 1/sample_rate)
+    fft_freq = fftfreq(len(data_read), 1 / sample_rate)
     plt.plot(fft_freq, np.abs(fft_out))
-    plt.title("Transformada de Fourier de la canción")
+    plt.title("Transformada de Fourier \n del sonido", fontdict={'size': 15})
     plt.xlabel("Frecuencia")
     plt.ylabel("|F(w)|")
     plt.show()
-    truncateGraphSong(fft_out, fft_freq, sample_rate)
+    return fft_out, fft_freq
 
 
-def invFftFileSong(fft_out, sample_rate, name_file):
+def truncated_sound_graph(fft_out, fft_freq):
+    # centro 36556+-1
+    fft_out[11346:61767] = 0
+    plt.plot(fft_freq, np.abs(fft_out))
+    plt.title("Transformada de Fourier \n del sonido (truncado)", fontdict={'size': 15})
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("|F(w)|")
+    plt.show()
+    return fft_out
+
+
+def inverse_fft_to_sound_file(fft_out, sample_rate, file_name):
     ifft_out = ifft(fft_out)
-    plt.plot(ifft_out.real)
-    plt.show()
-    waves.write(name_file, sample_rate, ifft_out.real)
-
-
-def truncateGraphSong(fft_out, fft_freq, sample_rate):
-    #centro 36556+-1
-    fft_out[16556:56557] = 0
-    plt.plot(fft_freq, np.abs(fft_out))
-    plt.title("Transformada de Fourier de la canción (truncado)")
-    plt.xlabel("Frecuencia")
-    plt.ylabel("|F(w)|")
-    plt.show()
-    invFftFileSong(fft_out, sample_rate, 'truncado.wav')
+    waves.write(file_name, sample_rate, ifft_out.real)
+    return ifft_out
 
 
 def main():
+    # Original Sound
     sample_rate, data_read = waves.read('handel.wav')
-    #print(sample_rate)
-    #print(len(data_read))
-    graphSong(data_read, sample_rate)
-    fftGraphSong(data_read, sample_rate)
+    sound_graph(data_read, sample_rate)
+
+    # FFT and IFFT
+    fft_out_1, fft_freq_1 = sound_freq_graph(data_read, sample_rate)
+    ifft_out_1 = inverse_fft_to_sound_file(fft_out_1, sample_rate, 'inversa.wav')
+    sound_graph(ifft_out_1.real, sample_rate)
+
+    # Truncated Sound and IFFT
+    fft_out_2 = truncated_sound_graph(fft_out_1, fft_freq_1)
+    ifft_out_2 = inverse_fft_to_sound_file(fft_out_2, sample_rate, 'truncado.wav')
+    sound_graph(ifft_out_2.real, sample_rate)
 
 
-
-# INGRESO
+# CALL MAIN
 main();
